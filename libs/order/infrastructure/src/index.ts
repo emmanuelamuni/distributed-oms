bba0e29309c568846986aa6bea028a99353b5265
akeematamuni <amuni.engr@gmail.com>
2026-06-24 16:06:21 +0100

    chore: barrel export for order infra

--- libs/order/infrastructure/src/index.ts
+++ libs/order/infrastructure/src/index.ts
@@ -1,1 +1,13 @@
 export * from './lib/order-infrastructure.module';
+
+// Messaging
+
+// Outbox
+export * from './lib/outbox/entities/order-outbox.typeorm-entity';
+export * from './lib/outbox/repositories/order-outbox.typeorm-repository';
+
+// Persistence
+export * from './lib/persistence/entities/order-line.typeorm-entity';
+export * from './lib/persistence/entities/order.typeorm-entity';
+export * from './lib/persistence/mappers/order.mapper';
+export * from './lib/persistence/repositories/order.typeorm-repository';
