2b58ad1bc9461426a809f0cd5582ae64aed66250
akeematamuni <amuni.engr@gmail.com>
2026-06-26 14:49:59 +0100

    chore: barrel export for inventory domain library

--- libs/inventory/domain/src/index.ts
+++ libs/inventory/domain/src/index.ts
@@ -3,11 +3,19 @@ export * from './lib/value-objects/quantity.value-object';
 
 // Exceptions
 export * from './lib/exceptions/insufficient-inventory.exception';
-export * from './lib/exceptions/inventory-unavailable.exception';
+export * from './lib/exceptions/reservation-already-exists.exception';
 export * from './lib/exceptions/reservation-not-found.exception';
 
 // Events
 export * from './lib/events/inventory-reserved.domain-event';
 export * from './lib/events/reservation-failed.domain-event';
 
-//
+// Aggregates & Entities
+export * from './lib/aggregates/inventory-node.aggregate';
+
+// Ports
+export * from './lib/ports/inventory-lock.port';
+export * from './lib/ports/inventory.repository.port';
+
+// Services
+export * from './lib/services/atp-calculator.service';
