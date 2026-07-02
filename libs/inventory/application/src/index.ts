7bf2a3062322f72cfbd181acfbb1824144ac9761
akeematamuni <amuni.engr@gmail.com>
2026-07-02 11:01:58 +0100

    chore: barrel exports of inventory application layer

--- libs/inventory/application/src/index.ts
+++ libs/inventory/application/src/index.ts
@@ -1,1 +1,15 @@
 export * from './lib/inventory-application.module';
+
+// Commands
+export * from './lib/commands/reserve-inventory.command';
+export * from './lib/commands/reserve-inventory.handler';
+export * from './lib/commands/release-reservation.command';
+export * from './lib/commands/release-reservation.handler';
+
+// Queries
+export * from './lib/queries/get-availability.handler';
+export * from './lib/queries/get-availability.query';
+
+// DTOs
+export * from './lib/dtos/availability-response.dto';
+export * from './lib/dtos/reserve-inventory.dto';
