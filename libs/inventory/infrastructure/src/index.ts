cc30a8478dd14f0df81fe5cc4cc981fcad264e2e
akeematamuni <amuni.engr@gmail.com>
2026-07-06 15:21:39 +0100

    chore: inventory infrastructure barrel export

--- libs/inventory/infrastructure/src/index.ts
+++ libs/inventory/infrastructure/src/index.ts
@@ -1,1 +1,14 @@
 export * from './lib/inventory-infrastructure.module';
+
+// Persistence
+export * from './lib/persistence/entities/inventory-node.typeorm-entity';
+export * from './lib/persistence/entities/inventory-reservation.typeorm-entity';
+export * from './lib/persistence/mappers/inventory.mapper';
+export * from './lib/persistence/repositories/inventory.typeorm-repository';
+
+// Outbox
+export * from './lib/outbox/entities/inventory-outbox.typeorm-entity';
+export * from './lib/outbox/repositories/inventory-outbox.typeorm-repository';
+
+// Locking
+export * from './lib/locking/redis-inventory-lock.adapter';
