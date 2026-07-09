12942cd42a5ca858e25fc4ea1145f14378e96a1f
akeematamuni <amuni.engr@gmail.com>
2026-07-09 14:27:43 +0100

    feat: implementation of a shared kafka dynamic module

--- libs/shared/kafka/src/lib/shared-kafka.module.ts
+++ libs/shared/kafka/src/lib/shared-kafka.module.ts
@@ -1,8 +1,36 @@
-import { Module } from '@nestjs/common';
+import { Module, DynamicModule } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
-@Module({
-    controllers: [],
-    providers: [],
-    exports: [],
-})
-export class SharedKafkaModule {}
+export interface SharedKafkaModuleOptions {
+    clientId: string;
+}
+
+export const KAFKA_PRODUCER = Symbol('KAFKA_PRODUCER');
+
+@Module({})
+export class SharedKafkaModule {
+    static forRootAsync(options: SharedKafkaModuleOptions): DynamicModule {
+        return {
+            module: SharedKafkaModule,
+            imports: [
+                ClientsModule.registerAsync([
+                    {
+                        name: KAFKA_PRODUCER,
+                        inject: [ConfigService],
+                        useFactory: (config: ConfigService) => ({
+                            transport: Transport.KAFKA,
+                            options: {
+                                client: {
+                                    clientId: options.clientId,
+                                    brokers: config.get('KAFKA_BROKERS').split(','),
+                                },
+                            },
+                        }),
+                    },
+                ]),
+            ],
+            exports: [ClientsModule],
+        };
+    }
+}
