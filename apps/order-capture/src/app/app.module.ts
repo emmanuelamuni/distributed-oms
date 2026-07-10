3cce52326f47109f9649fd5a0e1a9379756fc89f
akeematamuni <amuni.engr@gmail.com>
2026-07-10 16:41:50 +0100

    feat: order capture module wiring

--- apps/order-capture/src/app/app.module.ts
+++ apps/order-capture/src/app/app.module.ts
@@ -3,15 +3,21 @@ import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ScheduleModule } from '@nestjs/schedule';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 
-import { SharedUtilsModule } from '@doms/shared/utils';
+import { SharedUtilsModule, AllExceptionFilter } from '@doms/shared/utils';
 import { SharedRedisModule } from '@doms/shared/redis';
 import { SharedKafkaModule } from '@doms/shared/kafka';
-import { IdempotencyInterceptor } from '@doms/shared/idempotency';
 import { CreateOrderHandler, GetOrderHandler } from '@doms/order/application';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+import {
+    IDEMPOTENCY_STORE,
+    IdempotencyStoreAdapter,
+    IdempotencyInterceptor,
+} from '@doms/shared/idempotency';
 
 import {
     OutboxProcessor,
@@ -57,9 +63,13 @@ import { ORDER_REPOSITORY } from '@doms/order/domain';
         CreateOrderHandler,
         GetOrderHandler,
 
+        { provide: APP_FILTER, useExisting: AllExceptionFilter },
+        { provide: APP_INTERCEPTOR, useExisting: IdempotencyInterceptor },
+
         { provide: ORDER_REPOSITORY, useClass: OrderTypeOrmRepository },
         { provide: OUTBOX_REPOSITORY, useClass: OrderOutboxTypeOrmRepository },
         { provide: OUTBOX_PUBLISHER, useClass: OutboxPublisherAdapter },
+        { provide: IDEMPOTENCY_STORE, useClass: IdempotencyStoreAdapter },
 
         AppService,
     ],
