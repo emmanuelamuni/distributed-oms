49711bb72e6594d71e86316e873a1b1a8f404170
akeematamuni <amuni.engr@gmail.com>
2026-07-10 13:00:27 +0100

    feat: turned findPending to use raw query to enable SKIP LOCKED

--- libs/order/infrastructure/src/lib/outbox/repositories/order-outbox.typeorm-repository.ts
+++ libs/order/infrastructure/src/lib/outbox/repositories/order-outbox.typeorm-repository.ts
@@ -22,27 +22,40 @@ export class OrderOutboxTypeOrmRepository implements IOutboxRepositoryPort {
 
     async save(
         record: Omit<OutboxRecord, 'createdAt' | 'publishedAt' | 'retryCount'>,
-        queryRunner?: unknown,
+        queryRunner: unknown,
     ): Promise<void> {
         await this.getRepository(queryRunner).save(record);
     }
 
-    async findPending(limit: number, queryRunner?: unknown): Promise<OutboxRecord[]> {
-        return await this.getRepository(queryRunner).find({
-            where: { status: OutboxStatus.PENDING },
-            order: { createdAt: 'ASC' },
-            take: limit,
-        });
+    // SKIP LOCKED must be used to avoid collision
+    // order-capture and order-orchestrator both read and update this table
+    async findPending(limit: number, queryRunner: unknown): Promise<OutboxRecord[]> {
+        // return await this.getRepository(queryRunner).find({
+        //     where: { status: OutboxStatus.PENDING },
+        //     order: { createdAt: 'ASC' },
+        //     take: limit,
+        // });
+
+        const qr = queryRunner as QueryRunner;
+
+        return qr.manager.query(
+            `SELECT * FROM order_outboxes
+            WHERE status = 'PENDING'
+            ORDER BY created_at ASC
+            LIMIT $1
+            FOR UPDATE SKIP LOCKED`,
+            [limit],
+        );
     }
 
-    async markPublished(id: string, queryRunner?: unknown): Promise<void> {
+    async markPublished(id: string, queryRunner: unknown): Promise<void> {
         await this.getRepository(queryRunner).update(id, {
             status: OutboxStatus.PUBLISHED,
             publishedAt: new Date(),
         });
     }
 
-    async markFailed(id: string, queryRunner?: unknown): Promise<void> {
+    async markFailed(id: string, queryRunner: unknown): Promise<void> {
         await this.getRepository(queryRunner).update(id, {
             status: OutboxStatus.FAILED,
             retryCount: () => 'retry_count + 1',
