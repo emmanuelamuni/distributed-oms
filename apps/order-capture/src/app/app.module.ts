d504dd92234e54341b24a2e3c53223572118f750
akeematamuni <amuni.engr@gmail.com>
2026-07-14 12:23:44 +0100

    refactor: removed idempotency interceptor from global wiring

--- apps/order-capture/src/app/app.module.ts
+++ apps/order-capture/src/app/app.module.ts
@@ -3,21 +3,16 @@ import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CqrsModule } from '@nestjs/cqrs';
 import { ScheduleModule } from '@nestjs/schedule';
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER } from '@nestjs/core';
 
 import { SharedUtilsModule, AllExceptionFilter } from '@doms/shared/utils';
 import { SharedRedisModule } from '@doms/shared/redis';
 import { SharedKafkaModule } from '@doms/shared/kafka';
 import { CreateOrderHandler, GetOrderHandler } from '@doms/order/application';
+import { IDEMPOTENCY_STORE, IdempotencyStoreAdapter } from '@doms/shared/idempotency';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-import {
-    IDEMPOTENCY_STORE,
-    IdempotencyStoreAdapter,
-    IdempotencyInterceptor,
-} from '@doms/shared/idempotency';
 
 import {
     OutboxProcessor,
@@ -64,7 +59,6 @@ import { ORDER_REPOSITORY } from '@doms/order/domain';
         GetOrderHandler,
 
         { provide: APP_FILTER, useClass: AllExceptionFilter },
-        { provide: APP_INTERCEPTOR, useClass: IdempotencyInterceptor },
 
         { provide: ORDER_REPOSITORY, useClass: OrderTypeOrmRepository },
         { provide: OUTBOX_REPOSITORY, useClass: OrderOutboxTypeOrmRepository },
