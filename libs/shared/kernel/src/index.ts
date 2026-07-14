968c4a9ed0642902fc320bd9c2740a9a48f360c1
akeematamuni <amuni.engr@gmail.com>
2026-07-14 10:22:55 +0100

    refactor: updated kernel by removing the query base

--- libs/shared/kernel/src/index.ts
+++ libs/shared/kernel/src/index.ts
@@ -2,7 +2,6 @@ export * from './lib/aggregate-root.base';
 export * from './lib/command.base';
 export * from './lib/domain-event.base';
 export * from './lib/entity.base';
-export * from './lib/query.base';
 export * from './lib/repository.port';
 export * from './lib/use-case.port';
 export * from './lib/value-object.base';
--- libs/shared/kernel/src/lib/query.base.ts
+++
@@ -1,9 +0,0 @@
-import { randomUUID } from 'node:crypto';
-
-export abstract class QueryBase {
-    public readonly correlationId: string;
-
-    protected constructor(correlationId?: string) {
-        this.correlationId = correlationId ?? randomUUID();
-    }
-}
