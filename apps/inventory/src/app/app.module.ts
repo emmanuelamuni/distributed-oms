4573e41eabc0396a92408d5a1232ba05bad1104d
akeematamuni <amuni.engr@gmail.com>
2026-07-17 13:16:06 +0100

    feat: inventory application module wiring

---
+++ apps/inventory/src/app/app.module.ts
@@ -0,0 +1,79 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CqrsModule } from '@nestjs/cqrs';
+import { ScheduleModule } from '@nestjs/schedule';
+import { APP_FILTER } from '@nestjs/core';
+
+import { SharedUtilsModule, AllExceptionFilter } from '@doms/shared/utils';
+import { SharedRedisModule } from '@doms/shared/redis';
+import { IDEMPOTENCY_STORE, IdempotencyStoreAdapter } from '@doms/shared/idempotency';
+import { SharedKafkaModule } from '@doms/shared/kafka';
+
+import {
+    GetAvailabilityHandler,
+    ReserveInventoryHandler,
+    ReleaseReservationHandler,
+} from '@doms/inventory/application';
+
+import {
+    OutboxProcessor,
+    OUTBOX_REPOSITORY,
+    OUTBOX_PUBLISHER,
+    OutboxPublisherAdapter,
+} from '@doms/shared/outbox';
+
+import {
+    InventoryNodeTypeOrmEntity,
+    InventoryReservationTypeOrmEntity,
+    InventoryOutboxTypeOrmEntity,
+    InventoryTypeOrmRepository,
+    InventoryOutboxTypeOrmRepository,
+} from '@doms/inventory/infrastructure';
+
+import { INVENTORY_REPOSITORY } from '@doms/inventory/domain';
+
+import { InventoryConsumer } from './inventory.consumer';
+
+@Module({
+    imports: [
+        SharedUtilsModule,
+        SharedRedisModule,
+        SharedKafkaModule.forRootAsync({ clientId: 'inventory' }),
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                type: 'postgres',
+                url: config.get('INVENTORY_DB_URL'),
+                entities: [
+                    InventoryNodeTypeOrmEntity,
+                    InventoryReservationTypeOrmEntity,
+                    InventoryOutboxTypeOrmEntity,
+                ],
+                synchronize: false,
+            }),
+        }),
+        TypeOrmModule.forFeature([
+            InventoryNodeTypeOrmEntity,
+            InventoryReservationTypeOrmEntity,
+            InventoryOutboxTypeOrmEntity,
+        ]),
+        ScheduleModule.forRoot(),
+        CqrsModule,
+    ],
+    providers: [
+        GetAvailabilityHandler,
+        ReserveInventoryHandler,
+        ReleaseReservationHandler,
+        OutboxProcessor,
+        InventoryConsumer,
+
+        { provide: APP_FILTER, useClass: AllExceptionFilter },
+
+        { provide: INVENTORY_REPOSITORY, useClass: InventoryTypeOrmRepository },
+        { provide: OUTBOX_REPOSITORY, useClass: InventoryOutboxTypeOrmRepository },
+        { provide: OUTBOX_PUBLISHER, useClass: OutboxPublisherAdapter },
+        { provide: IDEMPOTENCY_STORE, useClass: IdempotencyStoreAdapter },
+    ],
+})
+export class AppModule {}
