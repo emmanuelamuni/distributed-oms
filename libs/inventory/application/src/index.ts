3edfa4029958274fd7601f591a1ede8771634e59
emmanuelamuni <amuni.engr@gmail.com>
2026-07-23 14:33:53 +0100

    chore: removed obsolete inventory query and handler

--- libs/inventory/application/src/index.ts
+++ libs/inventory/application/src/index.ts
@@ -4,10 +4,6 @@ export * from './lib/commands/reserve-inventory.handler';
 export * from './lib/commands/release-reservation.command';
 export * from './lib/commands/release-reservation.handler';
 
-// Queries
-export * from './lib/queries/get-availability.handler';
-export * from './lib/queries/get-availability.query';
-
 // DTOs
 export * from './lib/dtos/availability-response.dto';
 export * from './lib/dtos/reserve-inventory.dto';
